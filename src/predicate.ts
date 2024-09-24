0269c5648e32649fbc08549b599c7cf5067af5aa
Harsh Bajpai <bajpaiharsh244@gmail.com>
2024-09-24 17:20:33 +0530

    dev: remove old predicate and scripts

--- src/predicate.ts
+++ src/predicate.ts
@@ -1,7 +1,9 @@
-import { Address, bn, Provider, ScriptTransactionRequest, sha256, Signer, Wallet } from "fuels";
+import { Address, B256Coder, bn, createAssetId, Provider, ScriptTransactionRequest, sha256, Signer, Wallet } from "fuels";
 import { config } from "dotenv"
-import { calculatePayloadHash } from "./lib";
+import { calculaatePayloadHashNew, calculatePayloadHash } from "./lib";
 import { GaslessWallet } from "./predicates";
+import type { OutputCoinInput, TxInputInput } from "./predicates/scripts/DbgExample";
+import type { TxOutputInput } from "./predicates/predicates/GaslessWallet";
 
 config();
 
@@ -23,66 +25,109 @@ if (!PRIVATE_KEY) {
     process.exit(1);
 }
 
+const STABLE_COIN_CONTRACT_ADDRESS = process.env.STABLE_COIN_CONTRACT_ADDRESS;
+if (!STABLE_COIN_CONTRACT_ADDRESS) {
+    throw new Error('STABLE_COIN_CONTRACT_ADDRESS is not defined in the environment variables.');
+}
+
+const defaultSubId = "0x0000000000000000000000000000000000000000000000000000000000000000"
+const predicateAssetId = createAssetId(STABLE_COIN_CONTRACT_ADDRESS, defaultSubId);
+
 const wallet = Wallet.fromPrivateKey(PRIVATE_KEY, provider);
+const signer = new Signer(PRIVATE_KEY);
+
+const recipientAddress = wallet.address;
 
 const coins  = (await wallet.getCoins()).coins;
-console.log("coins are,", coins);
+const gasInputCoin = coins.find((coin) => {
+    return coin.assetId === provider.getBaseAssetId();
+})
+
+if (!gasInputCoin) {
+    throw new Error('No valid input coin found for gas payment.');
+}
+
+console.log("valid input coin for gas,", gasInputCoin);
 
 const scriptTransaction = new ScriptTransactionRequest({
-    gasLimit: 500000,
-    maxFee: 1000,
+    gasLimit: 1000000,
+    maxFee: 50000,
 });
 
-const gaslessPredicate = new GaslessWallet(wallet);
-gaslessPredicate.predicateData[0] = [0];
-gaslessPredicate.predicateData[1] = [0];
+const gaslessPredicate = new GaslessWallet({
+    provider, configurableConstants: {PUBLIC_KEY: wallet.publicKey }
+});
 
-
-console.log("predicate address: ", gaslessPredicate.address);
+const gaslessPredicateAddress = gaslessPredicate.address;
+console.log("predicate address: ", gaslessPredicateAddress.toAddress());
 
 const predicateCoins = (await provider.getCoins(gaslessPredicate.address)).coins;
 
-// NOTE: This line just adds the output coin
-gaslessPredicate.addTransfer(scriptTransaction, {
-   destination: wallet.address,
-   amount: bn(10),
-   assetId: predicateCoins[0].assetId
+const predicateInputCoin = predicateCoins.find((coin)=>{
+    return coin.assetId === predicateAssetId.bits
 });
-
-scriptTransaction.addCoinInput(predicateCoins[0]);
-
-console.log('script transaction inputs here', scriptTransaction.inputs);
-console.log('script transaction outputs here', scriptTransaction.outputs);
-
-const payloadHash = calculatePayloadHash({request: scriptTransaction, inputIndexes: [0], outputIndexes: [0], scriptByteCodeHash: sha256(scriptTransaction.script)})
-const signer = new Signer(PRIVATE_KEY);
-const signature = signer.sign(payloadHash);
-gaslessPredicate.predicateData[2] = signature;
-gaslessPredicate.populateTransactionPredicateData(scriptTransaction);
-
-// const predicateCoins = (await (predicate.getCoins())).coins;
-console.log('predicate Coins are: ', predicateCoins);
-console.log('native asset id:', provider.getBaseAssetId());
-
-console.log('coins are', coins);
-scriptTransaction.addCoinInput(coins[0]);
-await wallet.populateTransactionWitnessesSignature(scriptTransaction);
-
-if (scriptTransaction.inputs[0].type === 0){
-    const input = scriptTransaction.inputs[0];
-    console.log("inputs: ", scriptTransaction.inputs);
-    console.log("outputs:", scriptTransaction.outputs);
-    console.log("witness:", scriptTransaction.witnesses);
+if (!predicateInputCoin) {
+    throw new Error('No valid input coin found for predicate.');
 }
 
+console.log("Valid input coin for predicate:", predicateInputCoin);
+
+const amount = bn(10);
+const assetId = predicateInputCoin.assetId;
+
+// NOTE: This line just adds the output coin
+gaslessPredicate.addTransfer(scriptTransaction, {
+    destination: recipientAddress,
+    amount,
+    assetId
+ });
+
+ const inputCoinTxId = predicateInputCoin.id.slice(0, predicateInputCoin.id.length - 4)
+ const inputCoinOutputIndex = Number.parseInt(predicateInputCoin.id.slice(predicateInputCoin.id.length -4), 16)
+
+
+ const inputTxs: Array<TxInputInput> = [
+    {InputCoin: {
+   tx_id: inputCoinTxId,
+   output_index: inputCoinOutputIndex
+ }}];
+
+
+ const expectedOutputs: Array<TxOutputInput> = [
+    {OutputCoin:{
+    to: { bits: recipientAddress.toHexString() },
+    amount,
+    asset_id: {
+      bits: assetId
+    }}
+  }];
+
+  const payloadHash = calculaatePayloadHashNew(inputTxs, expectedOutputs);
+  const signtare = signer.sign(payloadHash);
+
+
+gaslessPredicate.predicateData[0] = inputTxs;
+gaslessPredicate.predicateData[1] = expectedOutputs;
+gaslessPredicate.predicateData[2] = signtare;
+
+scriptTransaction.addCoinInput(predicateCoins[0]);
+gaslessPredicate.populateTransactionPredicateData(scriptTransaction);
+
 const estimations = await provider.estimatePredicates(scriptTransaction);
-// console.log('estimations', estimations);
-// return;
+
+scriptTransaction.addCoinInput(gasInputCoin);
+
+await wallet.populateTransactionWitnessesSignature(scriptTransaction);
+
+// console.log("outputs:", scriptTransaction.outputs);
+// console.log("witness:", scriptTransaction.witnesses);
 
 console.log('predicate size', gaslessPredicate.bytes.length);
 
 const response = await (await wallet.sendTransaction(scriptTransaction)).waitForResult();
-console.log("response: ", response);
+console.log("response: ", response.id);
+console.log("gas used", response.gasUsed);
+
 }
 
 main();
--- src/script.ts
+++ src/script.ts
@@ -1,98 +1,154 @@
-import { Address, B256Coder, B512Coder, B512Coder, BigNumberCoder, bn, NumberCoder, Provider, Script, ScriptTransactionRequest, sha256, Signer, uint64ToBytesBE, Wallet } from "fuels";
-import {config} from "dotenv"
-import {DbgExample} from "./predicates/scripts/index"
-import {writeFileSync, readFileSync} from "node:fs"
-import { calculatePayloadHash } from "./lib";
+import {
+  Address,
+  arrayify,
+  B256Coder,
+  B512Coder,
+  BigNumberCoder,
+  bn,
+  hexlify,
+  NumberCoder,
+  Provider,
+  Script,
+  ScriptTransactionRequest,
+  sha256,
+  Signer,
+  uint64ToBytesBE,
+  UtxoIdCoder,
+  Wallet,
+} from "fuels";
+import { config } from "dotenv";
+import { DbgExample } from "./predicates/scripts/index";
+import { writeFileSync, readFileSync } from "node:fs";
+import { calculaatePayloadHashNew, calculatePayloadHash } from "./lib";
+import type { OutputCoinInput, TxInputInput, TxOutputInput } from "./predicates/scripts/DbgExample";
 
 config();
 
 const main = async () => {
-
-// Create a provider.
-const LOCAL_FUEL_NETWORK = process.env.LOCAL_FUEL_NETWORK_URL;
-if (!LOCAL_FUEL_NETWORK) {
-    console.error('LOCAL_FUEL_NETWORK_URL is not defined in the environment variables.');
+  // Create a provider.
+  const LOCAL_FUEL_NETWORK = process.env.LOCAL_FUEL_NETWORK_URL;
+  if (!LOCAL_FUEL_NETWORK) {
+    console.error(
+      "LOCAL_FUEL_NETWORK_URL is not defined in the environment variables."
+    );
     process.exit(1);
-}
+  }
 
-const provider = await Provider.create(LOCAL_FUEL_NETWORK);
+  const provider = await Provider.create(LOCAL_FUEL_NETWORK);
 
-// Create our wallet (with a private key).
-const PRIVATE_KEY = process.env.PRIVATE_KEY;
-if (!PRIVATE_KEY) {
-    console.error('PRIVATE_KEY is not defined in the environment variables.');
+  // Create our wallet (with a private key).
+  const PRIVATE_KEY = process.env.PRIVATE_KEY;
+  if (!PRIVATE_KEY) {
+    console.error("PRIVATE_KEY is not defined in the environment variables.");
     process.exit(1);
-}
+  }
 
-const wallet = Wallet.fromPrivateKey(PRIVATE_KEY, provider);
+  const wallet = Wallet.fromPrivateKey(PRIVATE_KEY, provider);
 
-if (!process.env.RECIPIENT_ADDRESS) {
-    console.error('RECIPIENT_ADDRESS is not defined in the environment variables.');
-    process.exit(1);
-}
-const recipientAddress = Address.fromAddressOrString(process.env.RECIPIENT_ADDRESS);
+  const recipientAddress = wallet.address;
 
-const coins  = (await wallet.getCoins()).coins;
-console.log("coins are,", coins);
+  const coins = (await wallet.getCoins()).coins;
+  console.log("coins are,", coins);
 
-const script = new DbgExample(wallet);
-script.setConfigurableConstants({
-    PUBLIC_KEY: wallet.publicKey
-});
+  const script = new DbgExample(wallet);
+  script.setConfigurableConstants({
+    PUBLIC_KEY: wallet.publicKey,
+  });
 
-const scriptByteCodeHash = sha256(script.bytes);
+  const b256Coder = new B256Coder();
+
+  const signer = new Signer(PRIVATE_KEY);
 
 
-const signer = new Signer(PRIVATE_KEY);
-const sig = signer.sign(new Uint8Array([0]));
+  const amount = bn(10);
 
-// we are setting up random arguments, which we will reset before sending the transaction
-const tx =  script.functions.main([0],[0], sig);
-tx.callParams({gasLimit: 100000});
+  const inputCoin = coins.find((coin) => {
+    return coin.assetId === provider.getBaseAssetId() && coin.amount > amount;
+  });
 
-const request = await tx.getTransactionRequest();
-request.addCoinInput(coins[0]);
-request.addCoinOutput(recipientAddress, 10, coins[0].assetId);
+  if (!inputCoin) {
+    throw new Error("No suitable input coin found. Ensure you have sufficient funds.");
+  }
 
-writeFileSync("./tx.json",JSON.stringify(request.toTransaction()));
+  console.log('input coin:', inputCoin.id);
 
+  const assetId = inputCoin.assetId;
 
-const payloadHash = calculatePayloadHash({
-    request,
-    inputIndexes: [0],
-    scriptByteCodeHash,
-    outputIndexes: [1],
-})
-
-console.log("payloadHash", payloadHash);
-
-const signature = signer.sign(payloadHash);
-console.log('digital signature', signature);
-
-tx.setArguments([0],[1], signature);
-
-const call = await tx.call();
-
-console.log('call sent ...');
-const response = await call.waitForResult();
-
-console.log('tx_id: ', response.transactionId);
-console.log('return value: ', response.value);
-console.log("transaction logs", response.logs);
-console.log("payload_hash", payloadHash);
-console.log("public key", wallet.publicKey);
-
-console.log("shar [0]",sha256(new Uint8Array([0, 1])));
-console.log('gas used', response.gasUsed);
+  const inputCoinTxId = inputCoin.id.slice(0, inputCoin.id.length - 4)
+  const inputCoinOutputIndex = Number.parseInt(inputCoin.id.slice(inputCoin.id.length -4), 16)
 
 
-// const sig1 = signer.sign(payloadHash);
+  const inputTxs: Array<TxInputInput> = [{InputCoin: {
+    tx_id: inputCoinTxId,
+    output_index: inputCoinOutputIndex
+  }}];
 
-// console.log('signature:', sig1);
-// console.log("public key", signer.publicKey);
+  console.log('inputTxs:', inputTxs);
 
-// const recoverPublicKey = Signer.recoverPublicKey(payloadHash, sig1);
-// console.log('recoveredPublicKey', recoverPublicKey);
-}
+  const expectedOutputs: Array<TxOutputInput> = [
+    {OutputCoin:{
+    to: { bits: recipientAddress.toHexString() },
+    amount,
+    asset_id: {
+      bits: assetId
+    }}
+  }];
+
+  const payloadHash = calculaatePayloadHashNew(inputTxs, expectedOutputs);
+  console.log('payload hash', payloadHash);
+
+  const signature = signer.sign(payloadHash);
+
+  console.log('script inputs for outputs: ', expectedOutputs);
+
+  // we are setting up random arguments, which we will reset before sending the transaction
+  const tx = script.functions.main(inputTxs, expectedOutputs, signature);
+  tx.callParams({ gasLimit: 500000 });
+
+  const request = await tx.getTransactionRequest();
+  request.addCoinInput(inputCoin);
+  request.addCoinOutput(recipientAddress, amount, assetId);
+  request.addCoinOutput(recipientAddress, amount, assetId);
+
+  writeFileSync("./tx.json", JSON.stringify(request.toTransaction()));
+
+//   const payloadHash = calculatePayloadHash({
+//     request,
+//     inputIndexes: [0],
+//     outputIndexes: [1],
+//   });
+
+//   console.log("payloadHash", payloadHash);
+
+//   const signature = signer.sign(payloadHash);
+//   console.log("digital signature", signature);
+
+//   tx.setArguments([0], [1], signature);
+
+  const call = await tx.call();
+
+  console.log("call sent ...");
+  const response = await call.waitForResult();
+
+  console.log("tx_id: ", response.transactionId);
+  console.log("return value: ", response.value);
+  console.log("transaction logs", response.logs);
+//   console.log("payload_hash", payloadHash);
+//   console.log("public key", wallet.publicKey);
+
+//   console.log("shar [0]", sha256(new Uint8Array([0, 1])));
+  console.log("gas used", response.gasUsed);
+  console.log("siganture", signature);
+  console.log("payload_hash", payloadHash);
+
+
+  // const sig1 = signer.sign(payloadHash);
+
+  // console.log('signature:', sig1);
+  // console.log("public key", signer.publicKey);
+
+  // const recoverPublicKey = Signer.recoverPublicKey(payloadHash, sig1);
+  // console.log('recoveredPublicKey', recoverPublicKey);
+};
 
 main();
